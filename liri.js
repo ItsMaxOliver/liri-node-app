b96ea0d91093bbfee7223d5c456161a74b761d28
MollyLJohnson <mollyl.johnson@att.net>
2018-03-31 15:41:58 -0500

    can retrieve tweets from twitter

--- liri.js
+++ liri.js
@@ -1,6 +1,17 @@
 require("dotenv").config();
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var Request = require("request");
 
-require("./keys.js");
+var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 var twitter = new Twitter(keys.twitter);
+
+var screenName = {screen_name: 'whutturfacts'};
+
+twitter.get("statuses/user_timeline", screenName, function(error, tweets, response) {
+    if (!error) {
+    console.log(JSON.stringify(tweets, null, 2)); //tweet body
+    }
+});
