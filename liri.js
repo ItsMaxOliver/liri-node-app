5775cc803fe54627021ec7d9aa363c290bff317e
MollyLJohnson <mollyl.johnson@att.net>
2018-04-02 18:39:41 -0500

    finished up the functionality of the app

--- liri.js
+++ liri.js
@@ -13,71 +13,82 @@ var twitter = new Twitter(keys.twitter);
 
 var screenName = {screen_name: 'ConanOBrien'};
 
-switch (command) {
-    case 'my-tweets':
-        twitter.get("statuses/user_timeline", screenName, function(error, tweets, response) {
-            if (!error) {
-                for(var i = 0; i < 20; i++) {
-                console.log("@ConanOBrien: " +(JSON.stringify(tweets[i].text, null, 2))); //tweet text
-                console.log("tweeted on " +(JSON.stringify(tweets[i].created_at, null, 2)) + "\n"); //tweet created at
+function doTheThing() {
+    switch (command) {
+        case 'my-tweets':
+            twitter.get("statuses/user_timeline", screenName, function(error, tweets, response) {
+                if (!error) {
+                    for(var i = 0; i < 20; i++) {
+                    console.log("@ConanOBrien: " +(JSON.stringify(tweets[i].text, null, 2))); //tweet text
+                    console.log("tweeted on " +(JSON.stringify(tweets[i].created_at, null, 2)) + "\n"); //tweet created at
+                    }
                 }
+            });
+        break;
+
+        case 'spotify-this-song':
+            spotify.search({type: "track", query: input, limit: 1}, function(err, info) {
+                if (!err) {
+                console.log("\nArtist: " + JSON.stringify(info.tracks.items[0].album.artists[0].name));
+                console.log("\nSong: " + input);
+                    if (JSON.stringify(info.tracks.items[0].preview_url) === "null"){
+                        console.log("\nSorry, no preview url found.")
+                    }
+                    else {
+                    console.log("\nPreview URL: " + JSON.stringify(info.tracks.items[0].preview_url));
+                    }
+                console.log("\nAlbum Name: " + JSON.stringify(info.tracks.items[0].album.name));
+                }
+                else{
+                    console.log(err);
+                }
+            });
+        break;
+
+        case 'movie-this':
+            if (input) {
+                request("http://www.omdbapi.com/?t=" + input + "&r=json&plot=short&apikey=trilogy")
+
+                .then(response => {
+                let data = JSON.parse(response);
+                console.log("\nTitle: " + data.Title);
+                console.log("\nYear: " + data.Year);
+                console.log("\nIMDB Rating: " + data.imdbRating);
+                console.log("\nRotten Tomatoes Rating: " + data.Ratings[1].Value);
+                console.log("\nCountry of Production: " + data.Country);
+                console.log("\nLanguage: " + data.Language);
+                console.log("\nPlot: " + data.Plot);
+                console.log("\nActors: " + data.Actors);
+                })
             }
-        });
-    break;
-        
-    case 'spotify-this-song':
-        spotify.search({type: "track", query: input, limit: 1}, function(err, info) {
-            if (!err) {
-            //console.log(info.tracks.items.artists.name);
-            console.log(input);
-            //console.log(info.tracks.items.preview_url);
-            //console.log(info.tracks.items.album.name);
-            }
-            else{
-                console.log(err);
-            }
-        });
-    break;
-        
-    case 'movie-this':
-        if (input) {
-            request("http://www.omdbapi.com/?t=" + input + "&r=json&plot=short&apikey=trilogy")
-            
-            .then(response => {
-            let data = JSON.parse(response);
-            console.log("Title: " + data.Title);
-            console.log("Year: " + data.Year);
-            console.log("IMDB Rating: " + data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + data.Ratings[1].Value);
-            console.log("Country of Production: " + data.Country);
-            console.log("Language: " + data.Language);
-            console.log("Plot: " + data.Plot);
-            console.log("Actors: " + data.Actors);
-            })
-        }
-        else {
-            request("http://www.omdbapi.com/?t=Mr. Nobody&r=json&plot=short&apikey=trilogy")
-            
-            .then(response => {
-            let data = JSON.parse(response);
-            console.log("Title: " + data.Title);
-            console.log("Year: " + data.Year);
-            console.log("IMDB Rating: " + data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + data.Ratings[1].Value);
-            console.log("Country of Production: " + data.Country);
-            console.log("Language: " + data.Language);
-            console.log("Plot: " + data.Plot);
-            console.log("Actors: " + data.Actors);
-            })
-        };
-    break;
-        
-    case 'do-what-it-says':
-        fs.readFile("random.txt", function(err, data){
-        if (err) {
-            return console.log(err);
-        }
-        // working code here
-    });
-    break;
+            else {
+                request("http://www.omdbapi.com/?t=Mr. Nobody&r=json&plot=short&apikey=trilogy")
+
+                .then(response => {
+                let data = JSON.parse(response);
+                console.log("\nTitle: " + data.Title);
+                console.log("\nYear: " + data.Year);
+                console.log("\nIMDB Rating: " + data.imdbRating);
+                console.log("\nRotten Tomatoes Rating: " + data.Ratings[1].Value);
+                console.log("\nCountry of Production: " + data.Country);
+                console.log("\nLanguage: " + data.Language);
+                console.log("\nPlot: " + data.Plot);
+                console.log("\nActors: " + data.Actors);
+                })
+            };
+        break;
+
+        case 'do-what-it-says':
+            fs.readFile("random.txt", "utf-8", function(err, data){
+                if (!err) {
+                    var splitData = data.split(", ", 2);
+                    command = splitData[0];
+                    input = splitData[1];
+                    doTheThing();
+                }
+            });
+        break;
+    }
 }
+
+doTheThing();
