10c010d761ace1557ba3cf26851974f95667b3e5
MollyLJohnson <mollyl.johnson@att.net>
2018-04-06 14:07:58 -0500

    added some error catches and put in placements if the user doesn't choose a song or movie

--- liri.js
+++ liri.js
@@ -11,37 +11,65 @@ var input = process.argv[3];
 var spotify = new Spotify(keys.spotify);
 var twitter = new Twitter(keys.twitter);
 
-var screenName = {screen_name: 'ConanOBrien'};
+var screenName = {screen_name: 'ChrissyTeigen'};
 
 function doTheThing() {
+    
     switch (command) {
+        
+        case undefined:
+            console.log(`
+Sorry no command was given. Please try again.
+
+The commands you can choose from are 
+    my-tweets
+    spotify-this-song "Song Title Goes Here"
+    movie-this "Movie Title Goes Here"
+    do-what-it-says`);
+        break;
+            
         case 'my-tweets':
-            twitter.get("statuses/user_timeline", screenName, function(error, tweets, response) {
+            twitter.get("statuses/user_timeline", screenName, function(error, tweets) {
                 if (!error) {
                     for(var i = 0; i < 20; i++) {
-                    console.log("@ConanOBrien: " +(JSON.stringify(tweets[i].text, null, 2))); //tweet text
-                    console.log("tweeted on " +(JSON.stringify(tweets[i].created_at, null, 2)) + "\n"); //tweet created at
+                        console.log("\n@chrissyteigen: " +(JSON.stringify(tweets[i].text, null, 2))); 
+
+                        var datetime = tweets[i].created_at.slice(0, 16);
+                        var year = tweets[i].created_at.slice(-4);
+
+                        console.log("   tweeted on " + datetime + " " + year); 
                     }
+                }
+                else {
+                    console.log(error);
                 }
             });
         break;
 
         case 'spotify-this-song':
+            if(input === undefined || input === "" || input === " ") {
+                input = "Volcanic Love";
+            }
+            
             spotify.search({type: "track", query: input, limit: 1}, function(err, info) {
+                
                 if (!err) {
-                console.log("\nArtist: " + JSON.stringify(info.tracks.items[0].album.artists[0].name));
-                console.log("\nSong: " + input);
+                    console.log("\nArtist: " + JSON.stringify(info.tracks.items[0].album.artists[0].name));
+                    console.log("\nSong: " + input);
+                    
                     if (JSON.stringify(info.tracks.items[0].preview_url) === "null"){
                         console.log("\nSorry, no preview url found.")
                     }
                     else {
-                    console.log("\nPreview URL: " + JSON.stringify(info.tracks.items[0].preview_url));
+                        console.log("\nPreview URL: " + JSON.stringify(info.tracks.items[0].preview_url));
                     }
-                console.log("\nAlbum Name: " + JSON.stringify(info.tracks.items[0].album.name));
+                    
+                    console.log("\nAlbum Name: " + JSON.stringify(info.tracks.items[0].album.name));
                 }
-                else{
+                else {
                     console.log(err);
                 }
+                
             });
         break;
 
@@ -50,41 +78,49 @@ function doTheThing() {
                 request("http://www.omdbapi.com/?t=" + input + "&r=json&plot=short&apikey=trilogy")
 
                 .then(response => {
-                let data = JSON.parse(response);
-                console.log("\nTitle: " + data.Title);
-                console.log("\nYear: " + data.Year);
-                console.log("\nIMDB Rating: " + data.imdbRating);
-                console.log("\nRotten Tomatoes Rating: " + data.Ratings[1].Value);
-                console.log("\nCountry of Production: " + data.Country);
-                console.log("\nLanguage: " + data.Language);
-                console.log("\nPlot: " + data.Plot);
-                console.log("\nActors: " + data.Actors);
+                    let data = JSON.parse(response);
+                    console.log("\nTitle: " + data.Title);
+                    console.log("\nYear: " + data.Year);
+                    console.log("\nIMDB Rating: " + data.imdbRating);
+                    if(data.Ratings[1] === undefined) {
+                        console.log("\nSorry no Rotten Tomatoes Rating found.")
+                    }
+                    else {
+                        console.log("\nRotten Tomatoes Rating: " + data.Ratings[1].Value);
+                    }
+                    console.log("\nCountry of Production: " + data.Country);
+                    console.log("\nLanguage: " + data.Language);
+                    console.log("\nPlot: " + data.Plot);
+                    console.log("\nActors: " + data.Actors);
                 })
             }
             else {
-                request("http://www.omdbapi.com/?t=Mr. Nobody&r=json&plot=short&apikey=trilogy")
+                request("http://www.omdbapi.com/?t=Black%20Panther&r=json&plot=short&apikey=trilogy")
 
                 .then(response => {
-                let data = JSON.parse(response);
-                console.log("\nTitle: " + data.Title);
-                console.log("\nYear: " + data.Year);
-                console.log("\nIMDB Rating: " + data.imdbRating);
-                console.log("\nRotten Tomatoes Rating: " + data.Ratings[1].Value);
-                console.log("\nCountry of Production: " + data.Country);
-                console.log("\nLanguage: " + data.Language);
-                console.log("\nPlot: " + data.Plot);
-                console.log("\nActors: " + data.Actors);
+                    let data = JSON.parse(response);
+                    console.log("\nTitle: " + data.Title);
+                    console.log("\nYear: " + data.Year);
+                    console.log("\nIMDB Rating: " + data.imdbRating);
+                    console.log("\nRotten Tomatoes Rating: " + data.Ratings[1].Value);
+                    console.log("\nCountry of Production: " + data.Country);
+                    console.log("\nLanguage: " + data.Language);
+                    console.log("\nPlot: " + data.Plot);
+                    console.log("\nActors: " + data.Actors);
                 })
             };
         break;
 
         case 'do-what-it-says':
-            fs.readFile("random.txt", "utf-8", function(err, data){
-                if (!err) {
+            fs.readFile("random.txt", "utf-8", function(error, data){
+                if (!error) {
                     var splitData = data.split(", ", 2);
                     command = splitData[0];
                     input = splitData[1];
                     doTheThing();
+                }
+                else {
+                    console.log(error);
                 }
             });
         break;
